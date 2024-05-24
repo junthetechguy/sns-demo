2182db6c78cccf216563f61ebee9a8167d3a0eee
junthetechguy <forestwoodleaves@gmail.com>
2024-05-24 15:08:58 +0900

    test: Done comment test cide

--- src/main/java/com/fastcampus/sns/controller/request/PostCommentRequest.java
+++ src/main/java/com/fastcampus/sns/controller/request/PostCommentRequest.java
@@ -1,12 +1,10 @@
 package com.fastcampus.sns.controller.request;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
 
-@Data
+@Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class PostCommentRequest {
     private String comment;
 }
